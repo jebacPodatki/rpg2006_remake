fbb1f905e94489072e176b85a53307fea4bf22be
dawid <dawid@dawid>
2022-09-30 02:24:12 +0200

    multiple target spells

--- system.py
+++ system.py
@@ -8,6 +8,7 @@ class Spell:
         self.dmg = [60, 90]
         self.effect = ''
         self.mp_cost = 50
+        self.single_target = True
 
 class CharacterSheet:
     def __init__(self):
@@ -41,6 +42,7 @@ class Library:
         spell2.effect = 'raise'
         spell2.impact = [0, 0]
         spell2.dmg = [0, 0]
+        spell2.single_target = False
         self.spells = {spell.name : spell, spell2.name : spell2}
         sheet = CharacterSheet()
         sheet.name = 'Skeleton'
@@ -80,6 +82,12 @@ class SpellHelper:
                 return False
         else:
             return False
+    def is_single_target(self, spell_name):
+        if spell_name in library.spells:
+            spell = library.spells[spell_name]
+            return spell.single_target
+        else:
+            return False 
 
 
 ACTION_NONE = 0
@@ -88,15 +96,15 @@ ACTION_MAGIC = 2
 ACTION_WAIT = 3
         
 class Action:
-    def __init__(self, type, actor : Character, target : Character, spell_name):
+    def __init__(self, type, actor : Character, targets, spell_name):
         self.type = type
         self.actor = actor
-        self.target = target
+        self.targets = targets
         self.spell_name = spell_name
         
 class ActionSelector:
     def select(self, character : Character, characterList, helper : SpellHelper):
-        return Action(ACTION_NONE, None, None, '')
+        return Action(ACTION_NONE, None, [], '')
     
 class AISelector(ActionSelector):
     def select(self, character : Character, character_list, helper : SpellHelper):
@@ -105,13 +113,13 @@ class AISelector(ActionSelector):
             if chr.faction != character.faction and chr.is_alive():
                 targets.append(chr)
         index = random.randint(0, len(targets) - 1)
-        return Action(ACTION_ATTACK, character, targets[index], '')
+        return Action(ACTION_ATTACK, character, [targets[index]], '')
             
 class ConsoleSelector(ActionSelector):
     def select(self, character : Character, characterList, helper : SpellHelper):
         print('\033[94m')
         print('Current character: ' + character.sheet.name)
-        print('Action: 1 - Attack, 2 - Magic, 3 - Wait')
+        print('Action: 1 - Attack, 2 - Magic, 3 - Wait.: ', end = " ")
         action = int(input())
         if action == 3:
             return Action(ACTION_WAIT, character, None, '')
@@ -124,41 +132,48 @@ class ConsoleSelector(ActionSelector):
                     msg += str(i) + ' - ' + spell + ', '
                     i += 1
             if i > 1:                
-                print(msg)
+                print(msg + ': ', end = " ")
                 selected_spell = character.sheet.spells[int(input()) - 1]
             else:
-                print("No spells")
+                print("No spells", end = " ")
                 action = 1
         targets = []
         for chr in characterList:
             if chr.faction != character.faction:
                 targets.append(chr)
-        msg = 'Choose target: '
-        i = 1
-        for target in targets:
-            if target.is_alive():            
-                msg += str(i) + ' - ' + target.sheet.name + ', '
-            i += 1
-        print(msg)
-        selected_target = targets[int(input()) - 1]
+        if action == 1 or helper.is_single_target(selected_spell) == True:
+            msg = 'Choose target: '
+            i = 1
+            for target in targets:
+                if target.is_alive():
+                    msg += str(i) + ' - ' + target.sheet.name + ', '
+                i += 1
+            print(msg + ': ', end = " ")
+            selected_targets = [targets[int(input()) - 1]]
+        else:
+            selected_targets = targets
                             
         if action == 1:
-            return Action(ACTION_ATTACK, character, selected_target, '')
+            return Action(ACTION_ATTACK, character, selected_targets, '')
         else:
-            return Action(ACTION_MAGIC, character, selected_target, selected_spell)
+            return Action(ACTION_MAGIC, character, selected_targets, selected_spell)
             
 class Logger:
-    def on_attack(self, attacker : Character, target : Character, damage):
+    def on_attack(self, attacker : Character, targets):
         pass
-    def on_block(self, attacker : Character, target : Character):
+    def on_damage(self, character : Character, damage):
         pass    
-    def on_magic(self, attacker : Character, target : Character, damage, effect, spell_name):
+    def on_block(self, character : Character):
         pass
-    def on_magic_block(self, attacker : Character, target : Character):
+    def on_cast_spell(self, attacker : Character, targets, spell_name):
+        pass
+    def on_spell_effect(self, targets, effect):
+        pass
+    def on_magic_block(self, character : Character):
         pass     
-    def on_wait(self, chr : Character):
+    def on_wait(self, character : Character):
         pass
-    def on_death(self, chr : Character):
+    def on_death(self, character : Character):
         pass
     def on_new_turn(self, current_character : Character, characters):
         pass
@@ -166,32 +181,36 @@ class Logger:
 COLOR = '\033[0m'
 COLOR2 = '\033[92m'
 class PrintLogger(Logger):
-    def on_attack(self, attacker : Character, target : Character, damage):
-        print(COLOR)
-        print(attacker.sheet.name + ' attacks ' + target.sheet.name + ' and deals ' + str(damage) + ' damage. ' + 
-              target.sheet.name + ' has ' + str(target.stats.hp) + ' HP now.')
-    def on_block(self, attacker : Character, target : Character):
-        print(COLOR)      
-        print(attacker.sheet.name + ' attacks ' + target.sheet.name + ' who blocks')        
-    def on_magic(self, attacker : Character, target : Character, damage, effect, spell_name):
-        print(COLOR)       
-        print(attacker.sheet.name + ' casts ' + spell_name + ' against ' + target.sheet.name + ' and deals ' + str(damage) + ' damage. ' + 
-              target.sheet.name + ' has ' + str(target.stats.hp) + ' HP now.')
-    def on_magic_block(self, attacker : Character, target : Character, spell_name):
-        print(COLOR)      
-        print(attacker.sheet.name + ' casts ' + spell_name + ' against ' + target.sheet.name + ' who effectively resists magic')      
-    def on_wait(self, chr : Character):
-        print(COLOR)        
-        print(chr.sheet.name + ' is waiting.')
-    def on_death(self, chr : Character):
-        print(COLOR)        
-        print(chr.sheet.name + ' died')
+    def on_attack(self, attacker : Character, targets):
+        print(COLOR, end = " ")
+        print(attacker.sheet.name + ' attacks ' + targets[0].sheet.name, end = " ")
+    def on_damage(self, character : Character, damage):
+        print('and deals ' + str(damage) + ' damage. ' + character.sheet.name + ' has ' + str(character.stats.hp) + ' HP now.')  
+    def on_block(self, character : Character):
+        print('who blocks')
+    def on_cast_spell(self, attacker : Character, targets, spell_name):
+        print(COLOR, end = " ")
+        if len(targets)  == 1:
+            print(attacker.sheet.name + ' casts ' + spell_name + ' against ' + targets[0].sheet.name, end = " ")
+        else:
+            print(attacker.sheet.name + ' casts ' + spell_name, end = ' ')
+    def on_spell_effect(self, targets, effect):
+        pass
+    def on_magic_block(self, character : Character):
+        print(COLOR, end = " ")
+        print('who effectively resists magic')
+    def on_wait(self, character : Character):
+        print(COLOR, end = " ")
+        print(character.sheet.name + ' is waiting.')
+    def on_death(self, character : Character):
+        print(COLOR, end = " ")
+        print(character.sheet.name + ' died')
     def on_new_turn(self, current_character : Character, characters):
         if current_character.controlled == False:
             return
-        print(COLOR2)        
+        print(COLOR2)
         for chr in characters:
-            print('\t' + chr.sheet.name + '\t\t' + '[' + str(chr.stats.hp) + '/' + str(chr.sheet.hp) + ']')        
+            print('\t' + chr.sheet.name + '\t\t' + '[' + str(chr.stats.hp) + '/' + str(chr.sheet.hp) + ']')
  
 class Fight:
     def __init__(self, characters, library : Library, selectorA : ActionSelector, selectorB : ActionSelector, logger):
@@ -204,7 +223,7 @@ class Fight:
         for chr in characters:
             self.alive[chr.faction] += 1
                 
-    def attack(self, attacker, target):
+    def attack_target(self, attacker, target):
         atk_factor = attacker.sheet.attack / target.sheet.defence
         atk = atk_factor * random.randint(attacker.sheet.breakage[0], attacker.sheet.breakage[1])        
         target.stats.dp -= atk        
@@ -212,34 +231,31 @@ class Fight:
             dmg_factor = attacker.sheet.strength / target.sheet.endurance
             dmg = dmg_factor * random.randint(attacker.sheet.dmg[0], attacker.sheet.dmg[1])
             target.stats.hp -= dmg
-            self.logger.on_attack(attacker, target, dmg)
+            self.logger.on_damage(target, dmg)
             if target.stats.hp < 0:
                 self.alive[target.faction] -= 1
                 self.logger.on_death(target)
             else:
                 target.stats.dp = target.sheet.dp
         else:
-            self.logger.on_block(attacker, target)
+            self.logger.on_block(target)
             
-    def magic(self, attacker, target, spell_name):
-        if spell_name in self.library.spells:
-            spell = self.library.spells[spell_name]
-        else:
-            return
-        if attacker.stats.mp < spell.mp_cost:
-            return
+    def attack(self, attacker, targets):
+        self.logger.on_attack(attacker, targets)
+        for target in targets:
+            self.attack_target(attacker, target)
+            
+    def magic_on_target(self, attacker, target, spell):
         attacker.stats.mp -= spell.mp_cost
         if spell.effect == 'raise':
             sheet = self.library.sheets['Skeleton']
-            #skeleton = Character(sheet, False, attacker.faction)
-            #self.characters.append(skeleton)
-            #self.alive[attacker.faction] += 1
             number = int(attacker.sheet.power / 5)
             if number > 0:
                 skeletons = [Character(sheet, False, attacker.faction) for i in range(number)]
                 for skeleton in skeletons:
                     self.characters.append(skeleton)
                 self.alive[attacker.faction] += number
+                self.logger.on_spell_effect(skeletons, spell.effect)
             return
         atk_factor = attacker.sheet.power / target.sheet.will
         atk = atk_factor * random.randint(spell.impact[0], spell.impact[1])
@@ -248,22 +264,33 @@ class Fight:
             dmg_factor = attacker.sheet.power / target.sheet.will
             dmg = dmg_factor * random.randint(spell.dmg[0], spell.dmg[1])
             target.stats.hp -= dmg
-            self.logger.on_magic(attacker, target, dmg, spell.effect, spell.name)
+            self.logger.on_damage(target, dmg)
             if target.stats.hp < 0:
                 self.alive[target.faction] -= 1
                 self.logger.on_death(target)
             else:
                 target.stats.rp = target.sheet.rp
         else:
-            self.logger.on_magic_block(attacker, target, spell.name)             
+            self.logger.on_magic_block(target)
+            
+    def magic(self, attacker, targets, spell_name):
+        if spell_name in self.library.spells:
+            spell = self.library.spells[spell_name]
+        else:
+            return
+        if attacker.stats.mp < spell.mp_cost:
+            return
+        self.logger.on_cast_spell(attacker, targets, spell.name)
+        for target in targets:
+            self.magic_on_target(attacker, target, spell)
                 
     def processAction(self, action : Action):
         if action.type == ACTION_WAIT:
             self.logger.on_wait(action.actor)
         elif action.type == ACTION_ATTACK:
-            self.attack(action.actor, action.target)
+            self.attack(action.actor, action.targets)
         elif action.type == ACTION_MAGIC:
-            self.magic(action.actor, action.target, action.spell_name)
+            self.magic(action.actor, action.targets, action.spell_name)
         else:
             pass
                 
