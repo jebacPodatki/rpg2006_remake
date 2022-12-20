556c4a7f47f3023c7d08785be3fafbb45248a3ef
dawid <dawid@dawid>
2022-12-20 01:47:33 +0100

    deserialize decorator should allow create a class instance without deserialization source

--- misc/decorators.py
+++ misc/decorators.py
@@ -12,8 +12,10 @@ class fromJson(object):
 class deserialize(object):
     def __call__(self, cls):
         class Wrapped(cls):
-            def __init__(self, source, *args):
+            def __init__(self, source = None, *args):
                 super().__init__(*args)
+                if source == None:
+                    return
                 properties = [i for i in super().__dict__.keys() if i[:1] != '_']
                 for prop in properties:
                     self.__dict__[prop] = source[prop]
